33afe62db173d3e350d9c5dbf9a47c2278aad373
mmmmmili <wywt789@outlook.com>
2021-05-07 09:57:01 +0800

    2021/5/7 9:56 1486 easy just to get familiar with xor ^

---
+++ leectcode/leectcode/1486. 数组异或操作.cpp
@@ -0,0 +1,25 @@
+//#include<iostream>
+//#include<stack>
+//#include <algorithm>
+//#include<math.h>
+//#include<vector>
+//using namespace std;
+//
+//
+//class Solution {
+//public:
+//    int xorOperation(int n, int start) {
+//        int ans = start;
+//        for (int i = 1; i < n; i++) {
+//            ans ^= start + 2 * i;
+//        }
+//        return ans;
+//        /*执行用时：0 ms, 在所有 C++ 提交中击败了100.00 % 的用户
+//            内存消耗：5.8 MB, 在所有 C++ 提交中击败了42.49 % 的用户*/
+//    }
+//};
+//
+//int main() {
+//    Solution b;
+//
+//}
